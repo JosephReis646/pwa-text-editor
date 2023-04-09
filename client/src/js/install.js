a5ba09cb9444349dfcac292da75f0acb298c4495
joseph reis <josephreis646@gmail.com>
2023-04-09 10:48:51 -0400

    Add an handler for the ppinstalled event

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -13,8 +13,17 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    
+    if (!deferredPrompt) return;
+
+  butInstall.style.display = 'none';
+  deferredPrompt.prompt();
+  const { outcome } = await deferredPrompt.userChoice;
+  console.log(`User accepted the installation prompt: ${outcome === 'accepted'}`);
+
+  deferredPrompt = null;
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('The application has been successfully installed.');
+});
